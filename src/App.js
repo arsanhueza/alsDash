4c7ee6fbf0e1a769731fd1f47ceb8ab5d83c51e4
Arturo Sanhueza <arsanhueza@udec.cl>
2021-09-13 22:24:21 -0300

    formato tabla

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
 import awsExports from "./aws-exports";
+import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
+import TableScrollbar from 'react-table-scrollbar';
+import randomusers from './data.js';
 
 Amplify.configure(awsExports);
 
@@ -44,38 +47,42 @@ const App = () => {
   return (
     <div style={styles.container}>
       <h2>Datos Guías</h2>
-      <input
-        onChange={event => setInput('nroguia', event.target.value)}
-        style={styles.input}
-        value={formState.nroguia}
-        placeholder="NroGuia"
-      />
-      <input
-        onChange={event => setInput('rutcliente', event.target.value)}
-        style={styles.input}
-        value={formState.rutcliente}
-        placeholder="RutCliente"
-      />
-      <button style={styles.button} onClick={addTodo}>Ingresar Guia</button>
-      {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.nroguia}</p>
-            <p style={styles.todoDescription}>{todo.rutcliente}</p>
-          </div>
-        ))
-      }
+          <div className="App">
+        <TableScrollbar>
+          {  <table>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Rut Cliente</th>
+        <th>Nro Guia</th>
+      </tr>
+    </thead>
+    <tbody>
+      {todos.map((user, id) =>
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{user.rutcliente}</td>
+          <td>{user.nroguia}</td>
+        </tr>
+      )}
+    </tbody>
+  </table>}
+        </TableScrollbar>
+      </div>
     </div>
   )
 }
 
+
+
 const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  container: { width: 2000, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
   todo: {  marginBottom: 15 },
   input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
   todoName: { fontSize: 20, fontWeight: 'bold' },
   todoDescription: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
+
 
 export default withAuthenticator(App)
