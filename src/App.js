b1c6f2adb6beeb3ed50794e4384be192fe287926
Arturo Sanhueza <arsanhueza@udec.cl>
2021-09-13 23:52:32 -0300

    se agregan colores

--- src/App.js
+++ src/App.js
@@ -42,13 +42,13 @@ const App = () => {
         <th>Peso Total</th>
         <th>Cliente</th>
         <th>Nro Bultos</th>
-        <th>Despacho</th>
+        <th>FechaDespacho</th>
     </tr>
     </thead>
     <tbody>
       {todos.map((user, id) =>
         <tr key={id}>
-          <td>{id + 1}</td>
+          <td>{id}</td>
           <td>{user.rutcliente}</td>
           <td>{user.nroguia}</td>
           <td>{user.estado}</td>
