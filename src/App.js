6d07fd76b34178fe236faa9de97bc52c7c54c73c
Arturo Sanhueza <arsanhueza@udec.cl>
2021-09-15 17:56:39 -0300

    cambio tabla, integración de logout

--- src/App.js
+++ src/App.js
@@ -1,79 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
-import { listTodos } from './graphql/queries'
-import awsExports from "./aws-exports";
+import React from 'react';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Home from './Home.js';
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import TableScrollbar from 'react-table-scrollbar';
 
-Amplify.configure(awsExports);
-
-const App = () => {
-  const [todos, setTodos] = useState([])
-
-  useEffect(() => {
-    fetchTodos()
-  }, [])
-
-  async function fetchTodos() {
-    try {
-     // const todoData = await API.graphql(
-     //            graphqlOperation(queries.listTodos, { filter: { title: { contains: title } } })
-     //          )
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
-  }
-
-
-
+function App() {
   return (
-    <div style={styles.container}>
-    <h2>Datos Guías</h2>
-    <div className="App">
-      <TableScrollbar> 
-          {  <table>
-    <thead>
-      <tr>
-        <th>id</th>
-        <th>Rut Cliente</th>
-        <th>Nro Guia</th>
-        <th>Estado</th>
-        <th>Peso Total</th>
-        <th>Cliente</th>
-        <th>Nro Bultos</th>
-        <th>FechaDespacho</th>
-        <th>Productos</th>
-    </tr>
-    </thead>
-    <tbody>
-      {todos.map((todo, id) =>
-        <tr key={id}>
-          <td>{id + 1}</td>
-          <td>{todo.rutcliente}</td>
-          <td>{todo.nroguia}</td>
-          <td>{todo.estado}</td>
-          <td>{todo.pesototal}</td>
-          <td>{todo.cliente}</td>
-          <td>{todo.nrobultos}</td>
-          <td>{todo.fechadespacho}</td>
-          <td>{todo.producto}</td>
-        </tr>
-      )}
-    </tbody>
-  </table>}
-        </TableScrollbar>
-      </div>
+    <div className="app">
+      <Home />
+      <AmplifySignOut />
     </div>
-      )
+  );
 }
 
-
-
-const styles = {
-  container: { width: 1200, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 }
-}
-
-
-export default withAuthenticator(App)
+export default withAuthenticator(App, true);
---
+++ src/Home.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { listTodos } from './graphql/queries'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const Home = () => {
+    const [todos, setTodos] = useState([])
+    const [formState, setFormState] = useState({ nroguia: '', rutcliente: '', estado: '', pesototal: '', cliente: '' ,fechadespacho: '', nrobultos: '', producto: ''})
+
+
+    useEffect(() => {
+        fetchTodos()
+
+    }, [])
+
+    const fetchTodos = async () => {
+        try {
+            const todoData = await API.graphql(graphqlOperation(listTodos))
+            const todos = todoData.data.listTodos.items
+            setTodos(todos)
+        } catch (err) { console.log('error fetching actors') }
+    }
+
+    const setInput = (key, value, isNumber = false) => {
+        value = (isNumber) ? parseInt(value) : value;
+        setFormState({ ...formState, [key]: value })
+    }
+
+    return (
+        <div className="home">
+            <div className="home__table">
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell >Nro Guía</TableCell>
+                                <TableCell >Rut Cliente</TableCell>
+                                <TableCell align="right">Estado</TableCell>
+                                <TableCell align="center">Peso Total</TableCell>
+                                <TableCell align="left">Cliente</TableCell>
+                                <TableCell >Fecha Despacho</TableCell>
+                                <TableCell >Nro Bultos</TableCell>
+                                <TableCell >producto</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {todos.map((row) => (
+                                <TableRow key={row?.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row?.nroguia}
+                                    </TableCell>
+                                    <TableCell >{row?.rutcliente}</TableCell>
+                                    <TableCell align="right">{row?.estado}</TableCell>
+                                    <TableCell >{row?.pesototal}</TableCell>
+                                    <TableCell >{row?.cliente}</TableCell>
+                                    <TableCell >{row?.fechadespacho}</TableCell>
+                                    <TableCell >{row?.nrobultos}</TableCell>
+                                    <TableCell >{row?.producto}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
